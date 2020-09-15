e852a8874152f8af291a3722cb522754f4031cbd
latishpavan <vc11@iitbbs.ac.in>
2020-09-15 20:10:43 +0530

    fixed binary operators on Point objects

--- kd_tree.py
+++ kd_tree.py
@@ -18,10 +18,10 @@ class Point:
         self.y = y
 
     def __sub__(self, other):
-        return self.x - other.x, self.y - other.y
+        return Point(self.x - other.x, self.y - other.y)
 
     def __add__(self, other):
-        return self.x + other.x, self.y + other.y
+        return Point(self.x + other.x, self.y + other.y)
 
     def __le__(self, other):
         return self.x < other.x or self.y < other.y
